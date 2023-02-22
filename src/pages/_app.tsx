f0fb605539c527283e54364e8bdd2c4810e79e25
chaitanyasjoshi <chaitanya9joshi@gmail.com>
2023-02-22 12:57:55 +0530

    remove unused boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
 import { WagmiConfig, createClient, configureChains } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { ConnectKitProvider, getDefaultClient } from "connectkit";
 import { polygonMumbai } from "wagmi/chains";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const { provider, webSocketProvider, chains } = configureChains(
   [polygonMumbai],
@@ -30,7 +33,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={client}>
       <ConnectKitProvider>
-        <Component {...pageProps} />
+        <main className={inter.className}>
+          <Component {...pageProps} />
+        </main>
       </ConnectKitProvider>
     </WagmiConfig>
   );
